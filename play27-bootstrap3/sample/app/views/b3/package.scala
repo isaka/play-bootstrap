a0030ecfe7e6b8f534fc1ab060b808836af2fe54
Isaka Traore <isaka@mimozar.com>
2024-07-13 14:48:37 +0200

    Delete play27-bootstrap3 directory

--- play27-bootstrap3/sample/app/views/b3/package.scala
+++
@@ -1,8 +0,0 @@
-package views.html.b3
-
-package object fc {
-  /**
-   * Returns it as a B4FieldConstructor to use it as default within a template
-   */
-  implicit val verticalFieldConstructor: B3FieldConstructor = my.vertical.fieldConstructor
-}
